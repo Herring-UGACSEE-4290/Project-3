2299c9b2bd47446c605e58ab1572636609a013eb
Michael Starks <starksmichael224@gmail.com>
2021-11-22 16:51:13 -0500

    fixed test arguments

--- tests/BabyTest.asm
+++ tests/BabyTest.asm
@@ -9,44 +9,44 @@ start:
 		MOV		R0, #0x0;
 		MOVT	R0, #0x0;
 		ADD		R6, R1, R2;
-		ADDS	R6, R1, R2; missing
+		ADDS	R6, R1, R2;
 		CMP		R5, R0, #0x1;
 		CMP		R6, R1, R2;
-		AND		R5, R0; missing
-		ANDS	R5, R0;
-		OR		R5, R0; missing
-		XOR		R5, R0;
-		XORS	R5, R0; missing
-		LSL		R0, #0x0; missing
-		LSR		R0, #0x0; missing
+		AND		R5, R0, R1; missing
+		ANDS	R5, R0, R1;
+		OR		R5, R0, R1; missing
+		XOR		R5, R0, R1;
+		XORS	R5, R0, R1; missing
+		LSL		R0, R0, #0x0; missing
+		LSR		R0, R0, #0x0; missing
 		CLR		R0;
-		Set		R0, #0x0; missing
-		NOT		R0; missing
+		Set		R0; missing
+		NOT		R0,R0; missing
 		B	#0x20;
-    	B.eq	Label, #0x20; missing
+    	B.eq	start; missing
     	BR  R0; missing
 		NOP;	
 test:
-		LOAD	R11, R10 ; 
-		STOR	R0, R2 ; 
+		LOAD	R11, R10
+		STOR	R0, R2
 		MOV		R0, #0x0;
 		MOVT	R0, #0x0;
 		ADD		R6, R1, R2;
-		ADDS	R6, R1, R2; missing
+		ADDS	R6, R1, R2;
 		CMP		R5, R0, #0x1;
 		CMP		R6, R1, R2;
-		AND		R5, R0; missing
-		ANDS	R5, R0;
-		OR		R5, R0; missing
-		XOR		R5, R0;
-		XORS	R5, R0; missing
-		LSL		R0, #0x0; missing
-		LSR		R0, #0x0; missing
+		AND		R5, R0, R1; missing
+		ANDS	R5, R0, R1;
+		OR		R5, R0, R1; missing
+		XOR		R5, R0, R1;
+		XORS	R5, R0, R1; missing
+		LSL		R0, R0, #0x0; missing
+		LSR		R0, R0, #0x0; missing
 		CLR		R0;
-		Set		R0, #0x0; missing
-		NOT		R0; missing
+		Set		R0; missing
+		NOT		R0,R0; missing
 		B	#0x20;
-    	B.eq	Label, #0x20; missing
+    	B.eq	start; missing
     	BR  R0; missing
 		NOP;
 		HALT;
