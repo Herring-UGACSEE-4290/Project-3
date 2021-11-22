ea98a9bac6be9cdb0850aacdfd284e619e396d61
Kevin Koffroth <ktkoffroth@gmail.com>
2021-11-22 15:01:26 -0500

    fixed BranchTest.asm

--- tests/BranchTest.asm
+++ tests/BranchTest.asm
@@ -1,23 +1,23 @@
 start:	
 		B	#0x20;
 
-    	B.eq	Label, #0x20;
-        B.ne    Label, #0x20;
-        B.cs    Label, #0x20;
-        B.hs    Label, #0x20;
-        B.cc    Label, #0x20;
-        B.lo    Label, #0x20;
-        B.mi    Label, #0x20;
-        B.pl    Label, #0x20;
-        B.vs    Label, #0x20;
-        B.vc    Label, #0x20;
-        B.hi    Label, #0x20;
-        B.ls    Label, #0x20;
-        B.ge    Label, #0x20;
-        B.lt    Label, #0x20;
-        B.gt    Label, #0x20;
-        B.le    Label, #0x20;
-        B.al    Label, #0x20;
+    	B.eq	test
+        B.ne    test
+        B.cs    test
+        B.hs    test
+        B.cc    test
+        B.lo    test
+        B.mi    test
+        B.pl    test
+        B.vs    test
+        B.vc    test
+        B.hi    test
+        B.ls    test
+        B.ge    test
+        B.lt    test
+        B.gt    test
+        B.le    test
+        B.al    test
 
     	BR  R0;
 
@@ -25,22 +25,22 @@ start:
 test:
 		B	#0x20;
 
-    	B.eq	Label, #0x20;
-        B.ne    Label, #0x20;
-        B.cs    Label, #0x20;
-        B.hs    Label, #0x20;
-        B.cc    Label, #0x20;
-        B.lo    Label, #0x20;
-        B.mi    Label, #0x20;
-        B.pl    Label, #0x20;
-        B.vs    Label, #0x20;
-        B.vc    Label, #0x20;
-        B.hi    Label, #0x20;
-        B.ls    Label, #0x20;
-        B.ge    Label, #0x20;
-        B.lt    Label, #0x20;
-        B.gt    Label, #0x20;
-        B.le    Label, #0x20;
-        B.al    Label, #0x20;
+    	B.eq	test
+        B.ne    test
+        B.cs    test
+        B.hs    test
+        B.cc    test
+        B.lo    test
+        B.mi    test
+        B.pl    test
+        B.vs    test
+        B.vc    test
+        B.hi    test
+        B.ls    test
+        B.ge    test
+        B.lt    test
+        B.gt    test
+        B.le    test
+        B.al    test
 
     	BR  R0;
