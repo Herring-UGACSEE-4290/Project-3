ed4537787d9a528d9a7c2e3422cb56c1398f76a2
PythonGuython <corrigank.matt@gmail.com>
2021-11-22 20:01:37 -0500

    created MoreTesting.asm

--- tests/MoreTesting.asm
+++ tests/MoreTesting.asm
@@ -4,4 +4,21 @@ lable1:
     ADD r0, r12, #0x3
     ADDS r0, r14, #0x5
 ;
+thingy:
+    CMP r1,r1, #0x40
+    AND r4,r3, #0x32
+    ANDS r4, r3, #0xa3
+    or r4,r15, #0x4a
+    ors r4,r15, #0x4a
+    not r0, r3
+    B lable1; check this for errors
+    B #0x0000
+    
+    BR r4, #0xa5
+    MOV32 r3, #0x0
+    RMB
+    XOR r1,r2,r3
+    XORS r1,r2,r3
+    FCB #0x04
 
+    HALT
