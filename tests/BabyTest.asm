b222a54a51b125f8b6bb4a96d86a905e4e5dafad
PythonGuython <corrigank.matt@gmail.com>
2021-11-22 19:28:48 -0500

    cleaned up babytest, added check for HALT, and removed parser calling a helper function twice

--- tests/BabyTest.asm
+++ tests/BabyTest.asm
@@ -6,47 +6,47 @@
 start:	
 		LOAD	R11, R10
 		STOR	R0, R2
-		MOV		R0, #0x0;
-		MOVT	R0, #0x0;
+		MOV		R0, #0x0
+		MOVT	R0, #0x0
 		ADD		R6, R1, R2;
 		ADDS	R6, R1, R2;
-		CMP		R5, R0, #0x1;
+		CMP		R5, R0, #0x1
 		CMP		R6, R1, R2;
-		AND		R5, R0, R1; missing
-		ANDS	R5, R0, R1;
-		OR		R5, R0, R1; missing
-		XOR		R5, R0, R1;
-		XORS	R5, R0, R1; missing
-		LSL		R0, R0, #0x0; missing
-		LSR		R0, R0, #0x0; missing
-		CLR		R0;
-		Set		R0; missing
-		NOT		R0,R0; missing
-		B	#0x20;
-    	B.eq	start; missing
-    	BR  R0; missing
-		NOP;	
+		AND		R5, R0, R1; 
+		ANDS	R5, R0, R1
+		OR		R5, R0, R1 
+		XOR		R5, R0, R1
+		XORS	R5, R0, R1 
+		LSL		R0, R0, #0x0
+		LSR		R0, R0, #0x0 
+		CLR		R0
+		Set		R0 
+		NOT		R0,R0 
+		B	#0x20
+    	B.eq	start 
+    	BR  R0 
+		NOP	
 test:
 		LOAD	R11, R10
 		STOR	R0, R2
-		MOV		R0, #0x0;
-		MOVT	R0, #0x0;
-		ADD		R6, R1, R2;
-		ADDS	R6, R1, R2;
-		CMP		R5, R0, #0x1;
-		CMP		R6, R1, R2;
-		AND		R5, R0, R1; missing
-		ANDS	R5, R0, R1;
-		OR		R5, R0, R1; missing
-		XOR		R5, R0, R1;
-		XORS	R5, R0, R1; missing
-		LSL		R0, R0, #0x0; missing
-		LSR		R0, R0, #0x0; missing
-		CLR		R0;
-		Set		R0; missing
-		NOT		R0,R0; missing
-		B	#0x20;
-    	B.eq	start; missing
-    	BR  R0; missing
-		NOP;
-		HALT;
+		MOV		R0, #0x0
+		MOVT	R0, #0x0
+		ADD		R6, R1, R2
+		ADDS	R6, R1, R2
+		CMP		R5, R0, #0x1
+		CMP		R6, R1, R2
+		AND		R5, R0, R1 
+		ANDS	R5, R0, R1
+		OR		R5, R0, R1 
+		XOR		R5, R0, R1
+		XORS	R5, R0, R1 
+		LSL		R0, R0, #0x0 
+		LSR		R0, R0, #0x0 
+		CLR		R0
+		Set		R0 
+		NOT		R0,R0 
+		B	#0x20
+    	B.eq	start 
+    	BR  R0 
+		NOP
+		HALT
---
+++ tests/MoreTesting.asm
@@ -0,0 +1,7 @@
+lable1:
+    MOV r2, #0x3
+    SUB r1, r3, #0x9
+    ADD r0, r12, #0x3
+    ADDS r0, r14, #0x5
+;
+
